c8885cad4941fc073282eb0a7a0310b92a8cad69
davidsdevel <davidsdevel@gmail.com>
2024-09-05 03:05:58 +0000

    refactor: Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: ['Quicksand', 'sans-serif'],
+      rubik: ['Rubik', 'sans-serif']
     },
+    extend: {
+      colors: {
+        main: {
+          100: '#a99efb',
+          300: '#837bbf',
+          500: '#5f4dee',
+          700: '#362e6f',
+          900: '#110c36'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
+
 export default config;
